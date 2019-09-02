5e888efae2c85023eb97297b6793cd806b77052a
Jeremy Thomerson <jeremy@thomersonfamily.com>
2019-09-02 00:28:08 -0400

    feat: major upgrade to update-code-folder script
    It is now reusable by other people, as the configuration for what you want
    cloned and where you want it is all stored in a config file.
    
    Handles both GitHub and Gitlab clones; for Gitlab, it supports an origin and a
    development fork.

--- bash/utilities.sh
+++ bash/utilities.sh
@@ -31,3 +31,46 @@ unsetPattern() {
    echo "Will unset: ${VARS}"
    unset ${VARS}
 }
+
+expandPath() {
+   # From https://stackoverflow.com/a/29310477
+   local path
+   local -a pathElements resultPathElements
+   IFS=':' read -r -a pathElements <<<"$1"
+   : "${pathElements[@]}"
+   for path in "${pathElements[@]}"; do
+      : "$path"
+      case $path in
+         "~+"/*)
+            path=$PWD/${path#"~+/"}
+            ;;
+         "~-"/*)
+            path=$OLDPWD/${path#"~-/"}
+            ;;
+         "~"/*)
+            path=$HOME/${path#"~/"}
+            ;;
+         "~"*)
+            username=${path%%/*}
+            username=${username#"~"}
+            IFS=: read -r _ _ _ _ _ homedir _ < <(getent passwd "$username")
+            if [[ $path = */* ]]; then
+               path=${homedir}/${path#*/}
+            else
+               path=$homedir
+            fi
+            ;;
+      esac
+      resultPathElements+=( "$path" )
+   done
+   local result
+   printf -v result '%s:' "${resultPathElements[@]}"
+   printf '%s\n' "${result%:}"
+}
+
+requireProgram() {
+   if ! hash "${1}"; then
+      echo "This script requires '${1}' be installed"
+      exit 1
+   fi
+}
--- scripts/update-code-folder.sh
+++ scripts/update-code-folder.sh
@@ -1,32 +1,116 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${SCRIPTDIR}/../bash/utilities.sh"
 
-updateRepo() {
-   REPONAME="$1"
-   ORGNAME="$2"
-   if [ -d "./${REPONAME}" ]; then
-      echo "${REPONAME} exists - fetching"
-      silentPushd "./${REPONAME}" && git fetch --all && silentPopd
+CONFIG_FILE="${HOME}/code/.code.json"
+if [ ! -z "${1}" ]; then
+   CONFIG_FILE="${1}"
+fi
+
+requireProgram "jq" # JSON parsing tool
+requireProgram "hub" # GitHub CLI tool
+
+echo "Using config: ${CONFIG_FILE}"
+
+getConfigValue() {
+   DIR_KEY="${1}"
+   CONFIG_KEY="${2}"
+   cat "${CONFIG_FILE}" | jq -r '.["'"${KEY}"'"] | .["'"${CONFIG_KEY}"'"]'
+}
+
+updateGitHubRepos() {
+   DIR="${1}"
+   QUERY="${2}"
+
+   [[ "${QUERY}" =~ orgs/([^/]+)/* ]] && ORG_NAME="${BASH_REMATCH[1]}"
+
+   mkdir -p "${DIR}"
+   silentPushd "${DIR}"
+   for REPO_NAME in $(hub api --paginate "${QUERY}" | jq -r '.[] | ((.fork | tostring) + " " + .name)' | grep '^false' | sed 's|^false ||'); do
+      updateGitHubRepo "${REPO_NAME}" "${ORG_NAME}"
+   done
+   silentPopd
+}
+
+updateGitHubRepo() {
+   REPO_NAME="${1}"
+   ORG_NAME="${2}"
+   if [ -d "./${REPO_NAME}" ]; then
+      echo "${REPO_NAME} exists - fetching"
+      silentPushd "./${REPO_NAME}" && git fetch --all && silentPopd
    else
-      echo "${REPONAME} does not exist - cloning"
-      if [ "${ORGNAME}" != "" ]; then
-         hub clone "${ORGNAME}/${REPONAME}"
+      echo "${REPO_NAME} does not exist - cloning"
+      if [ "${ORG_NAME}" != "" ]; then
+         hub clone "${ORG_NAME}/${REPO_NAME}"
       else
-         hub clone "${REPONAME}"
+         hub clone "${REPO_NAME}"
       fi
    fi
 }
 
-mkdir -p ~/code/jthomerson
-silentPushd ~/code/jthomerson
-for REPONAME in $(hub api --paginate 'user/repos?affiliation=owner' | jq -r '.[] | ((.fork | tostring) + " " + .name)' | grep '^false' | sed 's|^false ||'); do
-   updateRepo "${REPONAME}"
-done
-silentPopd
+updateGitlabRepos() {
+   DIR="${1}"
+   BASE_URL="${2}"
+   API_KEY="${3}"
+   GROUP="${4}"
+   DEV_GROUP="${5}"
 
-mkdir -p ~/code/silvermine
-silentPushd ~/code/silvermine
-for REPONAME in $(hub api --paginate 'orgs/silvermine/repos' | jq -r '.[] | ((.fork | tostring) + " " + .name)' | grep '^false' | sed 's|^false ||'); do
-   updateRepo "${REPONAME}" "silvermine"
-done
-silentPopd
+   mkdir -p "${DIR}"
+   silentPushd "${DIR}"
+
+   LIST_URL="${BASE_URL}/groups/${GROUP}/projects?per_page=100"
+   for PROJECT_ID in $(curl --insecure -s -H "PRIVATE-TOKEN: ${API_KEY}" "${LIST_URL}" | jq -r '.[].id'); do
+      PROJECT_FILE="/tmp/.gitlab-project-${PROJECT_ID}.json"
+      curl --insecure -s -H "PRIVATE-TOKEN: ${API_KEY}" "${BASE_URL}/projects/${PROJECT_ID}" > "${PROJECT_FILE}"
+      REPO_NAME=$(cat "${PROJECT_FILE}" | jq -r '.name')
+      CLONE_URL=$(cat "${PROJECT_FILE}" | jq -r '.ssh_url_to_repo')
+      if [ -d "./${REPO_NAME}" ]; then
+         echo "${REPO_NAME} exists - fetching"
+         silentPushd "./${REPO_NAME}" && git fetch --all && silentPopd
+      else
+         echo "${REPO_NAME} does not exist - cloning"
+         FORK_LIST="${BASE_URL}/groups/${DEV_GROUP}?per_page=100"
+         FORK_ID=$(curl --insecure -s -H "PRIVATE-TOKEN: ${API_KEY}" "${FORK_LIST}" | jq -r ".projects | map(select(.forked_from_project.id == ${PROJECT_ID}))[].id")
+         if [ "${FORK_ID}" == "" ]; then
+            echo "WARNING: No fork found!"
+            git clone "${CLONE_URL}"
+         else
+            git clone "${CLONE_URL}"
+            FORK_URL=$(curl --insecure -s -H "PRIVATE-TOKEN: ${API_KEY}" "${BASE_URL}/projects/${FORK_ID}" | jq -r '.ssh_url_to_repo')
+            if [ "${FORK_URL}" == "" ]; then
+               echo "URL for fork not found!"
+               exit 1
+            else
+               silentPushd "./${REPO_NAME}" && git remote add develop "${FORK_URL}" && git fetch --all && silentPopd
+            fi
+         fi
+      fi
+      rm "${PROJECT_FILE}"
+   done
+
+   silentPopd
+}
+
+processConfigFile() {
+   for KEY in $(cat "${CONFIG_FILE}" | jq -r '. | to_entries[].key'); do
+      TYPE=$(getConfigValue "${KEY}" "type")
+      DIR=$(expandPath $(getConfigValue "${KEY}" "dir"))
+
+      echo "Processing code folder ${KEY} (${TYPE}) into ${DIR}"
+      if [ "${TYPE}" == "github" ]; then
+         QUERY=$(getConfigValue "${KEY}" "query")
+         updateGitHubRepos "${DIR}" "${QUERY}"
+      elif [ "${TYPE}" == "gitlab" ]; then
+         BASE_URL=$(getConfigValue "${KEY}" "baseURL")
+         API_KEY=$(cat $(expandPath $(getConfigValue "${KEY}" "credentials")))
+         GROUP=$(getConfigValue "${KEY}" "group")
+         DEV_GROUP=$(getConfigValue "${KEY}" "developGroup")
+         updateGitlabRepos "${DIR}" "${BASE_URL}" "${API_KEY}" "${GROUP}" "${DEV_GROUP}"
+      else
+         echo "Unknown repo type '${TYPE}'"
+         exit 1
+      fi
+      echo
+   done
+}
+
+processConfigFile
